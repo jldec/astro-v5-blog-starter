067f4d8f6e632ba0aaf43261d2f8cf3bf89be734
jldec <jurgen@jldec.me>
2025-02-11 12:38:35 +0000

    doesn't work yet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-v5-blog-starter.jldec.me/',
+
   build: {
     format: 'file',
   },
+
   trailingSlash: 'never',
-  integrations: [tailwind()],
+  integrations: [],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
